a0e497be261c17e7c568f4e3d1faeace90b3df4a
DevSolex <devsolex6@gmail.com>
2025-07-16 19:32:24 +0000

    first commit

---
+++ Assign_operator.py
@@ -0,0 +1,23 @@
+# Arithmetic operators
+
+weekly_allowance = 2000
+print(f"my weekly allowance is {weekly_allowance}")
+print("i spend 400 for book")
+weekly_allowance -= 400
+print(f"new balance is {weekly_allowance}")
+print("i found 100 under my bed and added to my balance")
+weekly_allowance += 100
+print(f"my new balance turn {weekly_allowance}")
+print("i bought snacks worth 250")
+weekly_allowance -= 250
+print(f"my current balance is {weekly_allowance}")
+print("i gave 25% of my balance to my sibling")
+weekly_allowance -= 362.5
+print(f"my balance is now {weekly_allowance}")
+weekly_allowance -= 362.5 
+print(f"my new balance is {weekly_allowance}")
+weekly_allowance //= 2
+print(f"i split the balance into two and give half as tith now balance is {weekly_allowance}")
+weekly_allowance %= 100
+print(f"i take the modulus by 100 and my balance is now {weekly_allowance}")
+
---
+++ README.md
@@ -0,0 +1,19 @@
+#Am given a weekly allowance of ₦2000. and i am using Python assignment operators to simulate how i spend and manage the money throughout the week.
+
+✅ Instructions:
+
+Am writing a Python program that performs the following steps and prints the allowance after each one:
+
+I spent ₦400 on books.
+
+I found ₦100 under your bed and added it to your allowance.
+
+I bought snacks worth ₦250.
+
+I gave 25% of your current allowance to your younger sibling.
+
+I used one-third of what’s left to buy data.
+
+I split the remaining balance equally between savings and tithing.
+
+Finally,i calculate what remains after removing all complete ₦100 notes using the modulus assignment operator (%=).
